574cd764a0ef489708970c5be8221ccdc83d98fc
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-01 23:24:06 +1000

    feat: wrap App component in StoreProvider for Redux integration

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import Router from "./src/router";
+import { Provider as StoreProvider } from "react-redux";
+import store from "./src/store";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <Router />
-    </SafeAreaView>
+    <StoreProvider store={store}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+        <Router />
+      </SafeAreaView>
+    </StoreProvider>
   );
 }
 
