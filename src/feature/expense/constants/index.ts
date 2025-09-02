53569f59119bebfa5a79b9189f9125919ad59b54
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-02 17:49:22 +1000

    feat: add receipt URLs for Groceries and Rent in EXPENSE_DATA

--- src/feature/expense/constants/index.ts
+++ src/feature/expense/constants/index.ts
@@ -6,6 +6,8 @@ export const EXPENSE_DATA: Expense[] = [
     description: "Groceries",
     amount: 50,
     date: "2023-01-01T00:00:00Z",
+    receipt_url:
+      "https://media.gettyimages.com/id/1285504723/vector/receipt-of-sale.jpg?s=612x612&w=0&k=20&c=vCAuQvhBTFQEIwThmXNNuCyd-mjaIeME62KaSpehOCw=",
   },
   {
     _id: "2",
@@ -18,5 +20,7 @@ export const EXPENSE_DATA: Expense[] = [
     description: "Rent",
     amount: 1200,
     date: "2023-01-10T00:00:00Z",
+    receipt_url:
+      "https://media.gettyimages.com/id/1082996878/photo/close-up-of-grocery-receipt.jpg?s=612x612&w=0&k=20&c=22rtGosBeqS1MSS6dHhUyU7N8k51iwi99lFC2hTW5Jo=",
   },
 ];
