e4ecd6cfbda9fbe6c28425488a94ccf9265a19a3
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-02 22:35:07 +1000

    feat: enhance ExpenseForm component with initial values and onSubmit prop

--- src/feature/expense/components/ExpenseForm.tsx
+++ src/feature/expense/components/ExpenseForm.tsx
@@ -1,9 +1,41 @@
 import { Text, View } from "react-native";
+import { Expense } from "../typings";
+import Field from "../../../components/fields";
+import { FieldType } from "../../../components/fields/typings";
 
-export const ExpenseForm = () => {
+interface ExpenseFormProps {
+  initialValues?: Expense;
+  onSubmit: (values: Expense) => void;
+}
+
+export const ExpenseForm = ({ initialValues, onSubmit }: ExpenseFormProps) => {
   return (
     <View>
-      <Text>Add Expense</Text>
+      <Field
+        type={FieldType.DatePicker}
+        onChange={(date) => console.log(date)}
+        value={""}
+        label="Date"
+      />
+      <Field
+        type={FieldType.TextInput}
+        onChange={(text) => console.log(text)}
+        value={""}
+        label="Description"
+      />
+      <Field
+        type={FieldType.TextInput}
+        onChange={(text) => console.log(text)}
+        value={""}
+        label="Amount"
+      />
+      <Field
+        type={FieldType.Camera}
+        isMulti={false}
+        label="Receipt"
+        onCapture={(value) => console.log(value)}
+        value={""}
+      />
     </View>
   );
 };
