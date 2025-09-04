19260e84e84b908446f8850fd55c4bb2a86049c4
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-04 22:21:33 +1000

    feat: implement AuthContext, useAuth hook, and AuthProvider for authentication management

---
+++ src/feature/auth/provider/AuthProvider.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { AuthContext } from "../context";
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = async (username: string, password: string) => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
