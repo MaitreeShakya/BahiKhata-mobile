377cc3a7c5a6c390e86b5b36fe0ee70955731e15
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-04 22:22:03 +1000

    feat: implement navigation stacks for authentication and app screens

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,21 +2,35 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
+import { useAuth } from "../feature/auth/hooks/useAuth";
+import { AppStackParamList, AuthStackParamList } from "./typings";
 
-const Stack = createNativeStackNavigator();
+const AppStack = createNativeStackNavigator<AppStackParamList>();
+const AuthStack = createNativeStackNavigator<AuthStackParamList>();
 
-const RootStackScreen = () => {
+const AppStackScreen = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <AppStack.Navigator initialRouteName="HomeScreen">
+      <AppStack.Screen name="HomeScreen" component={HomeScreen} />
+    </AppStack.Navigator>
+  );
+};
+
+const AuthStackScreen = () => {
+  return (
+    <AuthStack.Navigator initialRouteName="LoginScreen">
+      <AuthStack.Screen name="LoginScreen" component={HomeScreen} />
+      <AuthStack.Screen name="RegisterScreen" component={HomeScreen} />
+    </AuthStack.Navigator>
   );
 };
 
 export default function Router() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <NavigationContainer>
-      <RootStackScreen />
+      {isAuthenticated ? <AppStackScreen /> : <AuthStackScreen />}
     </NavigationContainer>
   );
 }
--- src/router/typings.ts
+++ src/router/typings.ts
@@ -1,11 +1,21 @@
 import { RouteProp } from "@react-navigation/native";
 import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-export type RootStackParamList = {
-  Home: undefined;
+export type AppStackParamList = {
+  HomeScreen: undefined;
 };
 
-export type RootStackNavProps<T extends keyof RootStackParamList> = {
-  navigation: NativeStackNavigationProp<RootStackParamList, T>;
-  route: RouteProp<RootStackParamList, T>;
+export type AppStackNavProps<T extends keyof AppStackParamList> = {
+  navigation: NativeStackNavigationProp<AppStackParamList, T>;
+  route: RouteProp<AppStackParamList, T>;
+};
+
+export type AuthStackParamList = {
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
+};
+
+export type AuthStackNavProps<T extends keyof AuthStackParamList> = {
+  navigation: NativeStackNavigationProp<AuthStackParamList, T>;
+  route: RouteProp<AuthStackParamList, T>;
 };
