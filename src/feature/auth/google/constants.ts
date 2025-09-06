cb9f2111fbcf44c8aa8572945ddf41be891f1f5b
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-06 22:13:51 +1000

    feat: add Google Sign-In configuration for iOS client ID

---
+++ src/feature/auth/google/constants.ts
@@ -0,0 +1,5 @@
+import { ConfigureParams } from "@react-native-google-signin/google-signin";
+
+export const googleSignInConfig: ConfigureParams = {
+  iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+};
