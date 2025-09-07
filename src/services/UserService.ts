b3246246d7872ed48f751db52639021a678be620
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-07 20:58:38 +1000

    refactor: replace hardcoded user key with a static class property in UserService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,13 +2,15 @@ import { User } from "../feature/user/typings";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export class UserService {
+  private static USER_KEY: string = "user";
+
   static storeUserInAsyncStorage = async (user: User) => {
-    await AsyncStorage.setItem("user", JSON.stringify(user));
+    await AsyncStorage.setItem(this.USER_KEY, JSON.stringify(user));
   };
 
   static getUserFromAsyncStorage = async (): Promise<User> => {
     try {
-      const userString = await AsyncStorage.getItem("user");
+      const userString = await AsyncStorage.getItem(this.USER_KEY);
       if (userString) {
         const user: User = JSON.parse(userString);
         return user;
@@ -18,5 +20,9 @@ export class UserService {
     } catch (error) {
       throw error;
     }
+  };
+
+  static removeUserFromAsyncStorage = async () => {
+    await AsyncStorage.removeItem(this.USER_KEY);
   };
 }
