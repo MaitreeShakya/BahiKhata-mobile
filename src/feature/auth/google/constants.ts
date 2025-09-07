dd39691d6711bcf640e83e3779a4752c9d0d2778
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-07 14:41:04 +1000

    feat: update googleSignInConfig to include webClientId from environment variables

--- src/feature/auth/google/constants.ts
+++ src/feature/auth/google/constants.ts
@@ -2,4 +2,6 @@ import { ConfigureParams } from "@react-native-google-signin/google-signin";
 
 export const googleSignInConfig: ConfigureParams = {
   iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+  webClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
 };
+
