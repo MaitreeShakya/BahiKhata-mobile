715eefdea39ee57cb8a0f8ad966082948f309dff
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-07 22:39:05 +1000

    feat: enhance DropDownField component with improved layout and logging

--- src/components/fields/drop-down/index.tsx
+++ src/components/fields/drop-down/index.tsx
@@ -1,11 +1,43 @@
 import { Dropdown, MultiSelectDropdown } from "react-native-paper-dropdown";
 import { DropDownFieldProps } from "./typings";
+import { StyleSheet, View } from "react-native";
+import { Portal, Text } from "react-native-paper";
 
 export default function (props: DropDownFieldProps) {
   const { isMulti } = props;
-  if (isMulti) {
-    return <MultiSelectDropdown {...props} />;
-  } else {
-    return <Dropdown {...props} />;
-  }
+  console.log("Dropdown props:", props.options);
+  return (
+    <View style={styles.container}>
+      <Text variant="labelMedium" style={styles.label}>
+        {props.label}:
+      </Text>
+      {isMulti ? (
+        <MultiSelectDropdown
+          {...props}
+          mode="outlined"
+          menuContentStyle={styles.dropdown}
+        />
+      ) : (
+        <Dropdown
+          {...props}
+          mode="outlined"
+          menuContentStyle={styles.dropdown}
+        />
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+  },
+  label: {
+    color: "#000000",
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  dropdown: {
+    backgroundColor: "#ffffff",
+  },
+});
