df454cc49c93b03823280aa435a43d786d6173c1
DSA-Maitree <maitree.s@disastersolutionsaustralia.com.au>
2025-09-28 16:55:54 +1000

    feat: enhance dropdown functionality with multi-select support and improve UI interaction in DropDownField component

--- src/components/fields/drop-down/index.tsx
+++ src/components/fields/drop-down/index.tsx
@@ -1,11 +1,12 @@
-import { DropDownFieldProps } from "./typings";
-import { StyleSheet, View } from "react-native";
+import { DropDownFieldProps, DropdownOption } from "./typings";
+import { Pressable, StyleSheet, View } from "react-native";
 import { Text, TextInput } from "react-native-paper";
 import { useState } from "react";
 import Options from "./Options";
+import { SelectedOption } from "./SelectedOption";
 
 export default function (props: DropDownFieldProps) {
-  const { isMulti, options } = props;
+  const { options, isMulti, value, name, onSelect } = props;
   const [showDropDown, setShowDropDown] = useState<boolean>(false);
 
   const toggleDropDown = () => {
@@ -14,19 +15,82 @@ export default function (props: DropDownFieldProps) {
 
   const icon = showDropDown ? "chevron-right" : "chevron-down";
 
+  const handleSelect = (option: DropdownOption) => {
+    if (isMulti) {
+      const currentValue = value as string[];
+      const optionIndex = currentValue.indexOf(option.value);
+      if (optionIndex > -1) {
+        const newValue = currentValue.filter((val) => val !== option.value);
+        onSelect({ name, value: newValue });
+      } else {
+        onSelect({ name, value: [...currentValue, option.value] });
+      }
+    } else {
+      onSelect({ name, value: option.value });
+      setShowDropDown(false);
+    }
+  };
+
+  const handleRemoveSelected = (option: DropdownOption) => {
+    if (isMulti) {
+      const currentValue = value as string[];
+      const newValue = currentValue.filter((val) => val !== option.value);
+      onSelect({ name, value: newValue });
+    } else {
+      onSelect({ name, value: "" });
+    }
+  };
+
+  const renderSelectedValue = () => {
+    if (isMulti) {
+      const selectedOptions = options.filter((opt) =>
+        (value as string[]).includes(opt.value)
+      );
+      return selectedOptions.map((opt, index) => (
+        <SelectedOption
+          key={`selected-${index}`}
+          onRemove={handleRemoveSelected}
+          value={opt}
+        />
+      ));
+    } else {
+      const selectedOption = options.find((opt) => opt.value === value);
+      return (
+        selectedOption && (
+          <SelectedOption
+            onRemove={handleRemoveSelected}
+            value={selectedOption}
+          />
+        )
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text variant="labelMedium" style={styles.label}>
         {props.label}:
       </Text>
-      <TextInput
-        editable={false}
-        mode="outlined"
-        right={<TextInput.Icon icon={icon} />}
-        onPress={toggleDropDown}
-        style={styles.textInput}
-      />
-      {showDropDown && <Options options={options} onSelect={() => {}} onClose={toggleDropDown} />}
+      <Pressable onPress={toggleDropDown}>
+        <TextInput
+          editable={false}
+          mode="outlined"
+          right={<TextInput.Icon icon={icon} onPress={toggleDropDown} />}
+          style={styles.textInput}
+          placeholder="Select an option"
+        />
+      </Pressable>
+      {renderSelectedValue()}
+
+      {showDropDown && (
+        <Options
+          value={props.value}
+          isMulti={isMulti}
+          options={options}
+          onSelect={handleSelect}
+          onClose={toggleDropDown}
+        />
+      )}
     </View>
   );
 }
